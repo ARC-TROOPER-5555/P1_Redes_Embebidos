38a80e269342cb833a098119cee7b129ae53e2a2
Alejandra Hernandez <ie706675@iteso.mx>
2025-06-01 15:36:06 -0600

    Anadir libreria aes y driver App

---
+++ source/App.h
@@ -0,0 +1,18 @@
+/*
+ * App.h
+ *
+ *  Created on: 1 jun. 2025
+ *      Author: Usuario
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#ifndef APP_H_
+#define APP_H_
+
+static void phex(uint8_t* str);
+int decrypt_cbc(void);
+int encrypt_cbc(void);
+
+#endif /* APP_H_ */
